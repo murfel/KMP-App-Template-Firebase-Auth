6834e0f3ab85c5cfaf081c908db51c9e92bcf5cc
natasha <5155395+murfel@users.noreply.github.com>
2024-06-23 21:16:16 +0100

    remake login/logout screens into their own screens with proper DI and navigation

--- composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/App.kt
+++ composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/App.kt
@@ -3,7 +3,7 @@ package com.jetbrains.kmpapp
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
-import com.jetbrains.kmpapp.screens.LoginScreen
+import com.jetbrains.kmpapp.screens.auth.LoginScreen
 
 @Composable
 fun App() {
---
+++ composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/auth/AuthModel.kt
@@ -0,0 +1,6 @@
+package com.jetbrains.kmpapp.screens.auth
+
+import cafe.adriel.voyager.core.model.ScreenModel
+import dev.gitlive.firebase.auth.FirebaseAuth
+
+class AuthModel(val auth: FirebaseAuth) : ScreenModel
